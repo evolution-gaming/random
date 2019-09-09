c199116d014ebc304863b28d85d99d6de5744a42
Yaroslav Klymko <t3hnar@gmail.com>
2019-09-09 15:41:48 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 object Dependencies {
 
   val scalatest     = "org.scalatest"       %% "scalatest"   % "3.0.8"
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "0.0.18"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "0.0.29"
 
   object Cats {
     private val version = "1.6.1"
     val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
-    val effect = "org.typelevel" %% "cats-effect" % "1.3.1"
+    val effect = "org.typelevel" %% "cats-effect" % "1.4.0"
   }
 }
