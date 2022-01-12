62b48dd05c3559ae550dc94fc2e9067cbae884cb
Scala Steward <me@scala-steward.org>
2022-01-12 18:45:49 +0100

    Update cats-helper to 2.7.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "2.7.2"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "2.7.4"
   val scalatest     = "org.scalatest"       %% "scalatest"   % "3.1.1"
 
   object Cats {
