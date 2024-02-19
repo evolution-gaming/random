d97b114b55568cd54e15079b69cd7eb36b2a4090
Scala Steward <scala_steward@virtuslab.com>
2024-02-19 19:09:47 +0000

    Update cats-helper to 3.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.7.0"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.10.0"
   val scalatest     = "org.scalatest"       %% "scalatest"   % "3.2.16"
 
   object Cats {
